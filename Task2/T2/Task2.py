14d4f94368ea126ecfaba88c126f49d7fdedb975
agentvanya <agentvanya@gmail.com>
2016-07-02 16:11:28 +0300

    Task2 ThirdCommit

--- Task2/T2/Task2.py
+++ Task2/T2/Task2.py
@@ -1,10 +1,16 @@
+#import sys
 from random import uniform
-n=7
+#if len(sys.argv)>1:
+#    n=sys.argv[1]
+#else:
+#    n=0
+n=10
 i=0
+sum=0.0
 while i!=n:
     a=uniform(-1,1)
+    sum=sum+a
     print ("%.4f" % (a))
     i=i+1
-
-
-
+middle=sum/n
+print("middle = ","%.4f" % (middle))
